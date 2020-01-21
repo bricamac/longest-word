2b261eb805bc90087a9440c413e7034158c1124c
Jean-Pierre <jcornaglia@gmail.com>
2020-01-21 14:38:10 +0100

    ajout jpc

--- tests/test_game.py
+++ tests/test_game.py
@@ -18,7 +18,12 @@ class TestGame(unittest.TestCase):
         self.assertEqual(new_game.grid, list('ANLPIDFEW'))
     def test_is_invalid(self):
         new_game = Game()
-        new_game.grid = list('ANLPIDFEW') # Force the grid to a test case:
+        new_game.grid = list('ANLPIDFEW')
         self.assertIs(new_game.is_valid('MAISON'), False)
-        self.assertEqual(new_game.grid, list('ANLPIDFEW')) # Ma
+        self.assertEqual(new_game.grid, list('ANLPIDFEW'))
+    #Test dans un dico
+    def test_unknown_word_is_invalid(self):
+        new_game = Game()
+        new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
+        self.assertIs(new_game.is_valid('FEUN'), False)
 
